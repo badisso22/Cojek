24973190c8fc00be3fee304de19e6e4027622e33
Gina <64642337+Gina-bun@users.noreply.github.com>
2025-11-15 21:26:03 +0000

    Styled the dashboard and added icons

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -3,9 +3,9 @@ import "./Dashboard.css";
 export function Dashboard() {
   return (
     <div className="dashboard">
-      <div className="dashboard-container grid grid-cols-4  m-2">
+      <div className="dashboard-container grid grid-cols-6  m-2">
         {/* menu bar starts here */}
-        <div className="menu bg-amber-950 col-span-1 flex flex-col">
+        <div className="menu bg-amber-950 text-[#efe6e5] col-span-1 flex flex-col">
           {/* logo here */}
           <img
             className="size-28 pl-2"
@@ -14,39 +14,39 @@ export function Dashboard() {
           />
 
           {/* nav-links */}
-          <ul className="menu-links pl-6">
-            <Link to="">
-              <span>icon_here</span>
+          <ul className="menu-links pl-6 flex flex-col gap-3">
+            <Link className="flex" to="">
+              <img src="/images/home.png" alt="home icon" />
               <li>Home</li>
             </Link>
-            <Link to="">
-              <span>icon_here</span>
+            <Link className="flex" to="">
+              <img src="/images/save.png" alt="save icon" />
               <li>Saved</li>
             </Link>
-            <Link to="">
-              <span>icon_here</span>
+            <Link className="flex" to="">
+              <img src="/images/search.png" alt="search icon" />
               <li>Explore</li>
             </Link>
-            <Link to="">
-              <span>icon_here</span>
+            <Link className="flex" to="">
+              <img src="/images/profile.png" alt="profile icon" />
               <li>Profile</li>
             </Link>
-            <Link to="">
-              <span>icon_here</span>
+            <Link className="flex" to="">
+              <img src="/images/settings.png" alt="settings icon" />
               <li>Settings</li>
             </Link>
           </ul>
 
           {/* logout link */}
           <div className="logout flex mt-auto pl-6">
-            <span>icon_here</span>
+            <img src="/images/switch.png" alt="switch_icon" />
             <p>Logout</p>
           </div>
         </div>
         {/* menu bar ends here */}
 
         {/* dashboard area*/}
-        <div className="dashboard-area col-span-3 grid bg-amber-500">
+        <div className="dashboard-area col-span-5 grid max-h-[98vh] bg-amber-500">
           {/* search bar at the top */}
           <div className="search flex gap-2 m-auto max-w-[400px] ">
             {/* input + close button */}
@@ -56,9 +56,9 @@ export function Dashboard() {
                 type="text"
                 placeholder="Search"
               />
-              <p>x</p>
+              <img src="/images/close.png" alt="home icon" />
             </div>
-            <span className="border rounded-full p-2">icon</span>
+            <img className="size-10" src="/images/icon.png" alt="icon" />
           </div>
 
           {/* hero section */}
@@ -74,7 +74,7 @@ export function Dashboard() {
           </div>
 
           {/* streak section */}
-          <div className="streak-section bg-red-300 ">
+          <div className="streak-section min-w-[600px] flex justify-self-center bg-pink-600 mx-14 py-5 my-5 rounded-md justify-around ">
             {/* streak display */}
             <div className="streak-display">
               <h2>Your Streak</h2>
@@ -87,53 +87,59 @@ export function Dashboard() {
             <div className="project-count">
               <h3>Total Projects Completed</h3>
               <p>3</p>
-              <progress value="25" max="100"></progress>
-              <div className="user-level">
+              <progress className="flex-1" value="25" max="100"></progress>
+              <div className="user-level flex gap-1">
                 <p>Beginner</p>
                 <p>Intermediate</p>
                 <p>Advanced</p>
               </div>
             </div>
+            <div />
+          </div>
 
-            {/*project suggestion section */}
-            <div className="suggestion">
-              <div className="">
-                <h3>Programming for Beginners</h3>
-                <p>Practice the basics</p>
-                <button>Try this</button>
+          {/* streak ends here */}
+
+          {/*project suggestion section */}
+          <div className="suggestion py-10 px-6 grid">
+            <h2 className="text-center text-2xl font-bold pb-6">Suggested Projects</h2>
+            <div className="flex gap-3 m-auto">
+              <div className="bg-amber-600 py-3 px-3 text-center rounded-md">
+                <h3 className="font-bold py-2">Programming for Beginners</h3>
+                <p className="py-2 text-sm">Practice the basics</p>
+                <button className="border border-white py-1 px-5 rounded-[16px]">Try this</button>
               </div>
 
-              <div className="">
-                <h3>Programming for Beginners</h3>
-                <p>Practice the basics</p>
-                <button>Try this</button>
+              <div className="bg-amber-600 py-3 px-3 text-center rounded-md">
+                <h3 className="font-bold py-2">Programming for Beginners</h3>
+                <p className="py-2 text-sm">Practice the basics</p>
+                <button className="border border-white py-1 px-5 rounded-[16px]">Try this</button>
               </div>
 
-              <div className="">
-                <h3>Programming for Beginners</h3>
-                <p>Practice the basics</p>
-                <button>Try this</button>
+              <div className="bg-amber-600 py-3 px-3 text-center rounded-md">
+                <h3 className="font-bold py-2">Programming for Beginners</h3>
+                <p className="py-2 text-sm">Practice the basics</p>
+                <button className="border border-white py-1 px-5 rounded-[16px]">Try this</button>
+              </div>
+
+            </div>
+          </div>
+
+          {/* saved projects section */}
+          <div className="saved-projects">
+            <h3 className="font-bold text-2xl text-center">Your Saved Projects</h3>
+            <div className="project-progress flex justify-around max-w-[800px] m-auto">
+              <div className="flex flex-col">
+                <h3>Intro to Python</h3>
+                <progress value="60" max="100"></progress>
+                <button className="text-xs bg-amber-700 w-fit mt-1 rounded-md px-1 py-1">60%</button>
+              </div>
+
+              <div className="flex flex-col">
+                <h3>Intro to Java</h3>
+                <progress value="30" max="100"></progress>
+                <button className="text-xs bg-amber-700 w-fit mt-1 rounded-md px-1 py-1">30%</button>
               </div>
             </div>
-
-            {/* saved projects section */}
-            <div className="saved-projects">
-              <h2>Your Saved Projects</h2>
-              <div className="project-progress">
-                <div>
-                  <h3>Intro to Python</h3>
-                  <progress value="60" max="100"></progress>
-                  <button>60%</button>
-                </div>
-
-                <div>
-                  <h3>Intro to Java</h3>
-                  <progress value="30" max="100"></progress>
-                  <button>30%</button>
-                </div>
-              </div>
-            </div>
-
           </div>
         </div>
       </div>
