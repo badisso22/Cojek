97733769cd167fbf632f0a8f9098bb99c7c71b32
badisso <mehdi.moussous2162@gmail.com>
2025-11-16 09:41:24 +0100

    changed the code that was badly written by who ever did it
    nothing works , get better

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { Routes, Route } from 'react-router-dom';
 import { ChatbotInput } from './components/ChatbotInput';
 import { LandingPage } from './pages/LandingPage';
@@ -6,35 +5,21 @@ import { ChatMessages } from './components/ChatMessages';
 import { ProjectGenPage } from './pages/ProjectGenPage';
 import SignUpPage from './pages/SignUpPage';
 import LoginPage from './pages/LoginPage';
-
-=======
-
-
-
-import {Dashboard} from './pages/Dashboard'
->>>>>>> frontend-setup
-import './App.css'
+import { Dashboard } from './pages/Dashboard';
+import './App.css';
 
 function App() {
-
- 
-
   return (
-   <Routes>
-    <Route path="/" element={<LandingPage />} />
-    <Route path="/login" element={<LoginPage />} />
-    <Route path="/signup" element={<SignUpPage />} />
-    <Route path='/project-gen' element={<ProjectGenPage />} />
-<<<<<<< HEAD
-    <Route path='/chatbot-input' element={<ChatbotInput />}></Route>
-     <Route path='/chatbot-messages' element={<ChatMessages  />}></Route>
-
-=======
-    <Route path='/dashboard' element={<Dashboard/>}/>
-     
->>>>>>> frontend-setup
-   </Routes>
-  )
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignUpPage />} />
+      <Route path='/project-gen' element={<ProjectGenPage />} />
+      <Route path='/chatbot-input' element={<ChatbotInput />} />
+      <Route path='/chatbot-messages' element={<ChatMessages />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,39 +1,32 @@
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export function Navbar() {
-    return(
-        <div>
-            <nav className="flex mt-4 mx-5  bg-(--navbar-background) text-(--text-navbar) h-auto rounded-[20px]  px-10 items-center">
-        <ul className="flex gap-4 ">
+  return (
+    <div>
+      <nav className="flex mt-4 mx-5  bg-(--navbar-background) text-(--text-navbar) h-auto rounded-[20px]  px-10 items-center">
+        <ul className="flex gap-4">
           <Link className="py-1" to="/">
             Home
           </Link>
-
           <Link className="py-1" to="/about">
             About
           </Link>
-
           <Link className="py-1" to="/quiz">
             Quiz
           </Link>
-
-<<<<<<< HEAD
           <Link className="bg-(--login-button) rounded-[25px] px-4 py-1" to="/login">
-          Login
-          </Link>
-=======
-          <Link className="bg-(--login-button) rounded-[25px] px-4 py-1" to="/dashboard">
             Login
->>>>>>> frontend-setup
+          </Link>
+          <Link className="bg-(--login-button) rounded-[25px] px-4 py-1" to="/dashboard">
+            Dashboard
           </Link>
         </ul>
-
         <img
-          className="ml-auto  size-17 "
+          className="ml-auto size-17"
           src="/images/logo2.png"
           alt="Cojek logo"
         />
       </nav>
-        </div>
-    )
+    </div>
+  );
 }
